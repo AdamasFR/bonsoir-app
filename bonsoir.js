3b2ddab35c760755e383dcab8f7191ce748ab7b9
Anthony Dumas <anthony-dumas@pad-panic.fr>
2019-05-07 14:36:55 +0200

    Première version fonctionnelle : Renvoi uniquement le premier element défini

--- bonsoir.js
+++ bonsoir.js
@@ -2,21 +2,35 @@
  * Bonsoir ou Bonjour
  * - Renvoi la bonne interjection selon l'heure d'accès a l'api
  */
+
+/**
+ * Default Salutation
+ */
+class Salutation {
+      constructor(salutationFull, salutationShort, timeStart, timeStop) {
+            this.salutationFull = salutationFull != null ? salutationFull : "Hello";
+            this.salutationShort = salutationShort != null ? salutationShort : "hi";
+            this.timeStart = timeStart != null ? timeStart : "0000";
+            this.timeStop = timeStop != null ? timeStart : "0000";
+      }
+}
+
 class Bonsoir {
-      interjectionSoir = ["Bonsoir", "bsr", "1800", "0600"];
-      interjectionJournee = ["Bonjour", "bjr", "0600", "1800"];
-      interjectionsMap = [interjectionJournee, interjectionSoir];
-      locale = "FR";
+      constructor() {
+            this.salutationMorning = new Salutation("Bonjour", "bjr", "0600", "1100");
+            this.salutationMidDay = new Salutation("Bonjour", "bjr", "1100", "1800");
+            this.salutationTwilight = new Salutation("Bonsoir", "bsr", "1800", "0000");
+            this.salutationNight = new Salutation("Bonne Nuit", "bnt", "0000", "0600");
+            this.salutationMap = [this.salutationMorning, this.salutationMidDay, this.salutationTwilight, this.salutationNight];
+            this.locale = "FR";
+      }
+
       /**
-       * Point d'entrée
+       * Entry Point
        */
-      interjection() {
-            // interjection retournée par défaut
-            interjectionReturned = interjectionsMap[0].fullname;
-            return interjectionReturned;
+      salutation() {
+            return this.salutationMap[0].salutationFull;
       };
 };
 
 var bonsoir = new Bonsoir();
-
-console.log(bonsoir.interjection());
--- localization/bonsoir-en.js
+++ localization/bonsoir-en.js
@@ -3,22 +3,8 @@
  * - Returns Good Evening or Good day
  */
 class BonsoirEnglish extends Bonsoir {
-    interjectionDay = [{
-        fullname: "Good Day",
-        abbrv: "gd day",
-        startHour: "0600",
-        endHour: "1800"
-    }];
-    interjectionNight = [{
-        fullname: "Good Evening",
-        abbrv: "gd ev",
-        startHour: "1800",
-        endHour: "0600"
-    }];
-    interjectionsMap = [
-        [interjectionDay],
-        [interjectionNight]
-    ];
-    locale = "EN";
+    constructor() {
+        
+    }
 };
 var bonsoir = new BonsoirEnglish();
