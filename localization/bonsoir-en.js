2cf8d927f13a34dcc4c7400839956d7238f3cdc2
Anthony Dumas <anthony@dumas.cc>
2020-05-05 09:52:17 +0200

    Add English Support
    Based on https://fr.slideshare.net/april_anne/basic-greetings-throughout-the-day

--- localization/bonsoir-en.js
+++ localization/bonsoir-en.js
@@ -1,3 +1,4 @@
+// based on https://slideshare.net/april_anne/basic-greetings-throughout-the-day
 bonsoirEN = {
     "meta": {
         "version": "1.0",
@@ -6,10 +7,28 @@ bonsoirEN = {
     },
     "salutations": [
         {
-            "salutationFull": "Hello",
-            "salutationShort": "uwu",
-            "timeStart": 0,
-            "timeStop": 24
+            "salutationFull": "Good Morning",
+            "salutationShort": "gm",
+            "timeStart": 5,
+            "timeStop": 12
+        },
+        {
+            "salutationFull": "Good Afternoon",
+            "salutationShort": "ga",
+            "timeStart": 12,
+            "timeStop": 17
+        },
+        {
+            "salutationFull": "Good Evening",
+            "salutationShort": "good ev",
+            "timeStart": 17,
+            "timeStop": 20
+        },
+        {
+            "salutationFull": "Good Night",
+            "salutationShort": "gn",
+            "timeStart": 20,
+            "timeStop": 5
         }
     ]
 }
